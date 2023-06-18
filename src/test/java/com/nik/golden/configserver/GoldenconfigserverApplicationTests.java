532c1b857e6b520722538e738774d9dfa5780dca
Nikhil Bhatia <nikhil.bhatia.1310@gmail.com>
2023-06-18 21:18:03 +0530

    config server

---
+++ src/test/java/com/nik/golden/configserver/GoldenconfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nik.golden.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenconfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
